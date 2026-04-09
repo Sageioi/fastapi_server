09c0cacddd806a5c577fbebced2b54e79b2b1cca
Tega <tegaisikuru@gmail.com>
2026-04-09 10:01:55 -0900

    Changed Port

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import uvicorn as uvi
 def run_server(status = True):
    try:
     if status:
-        uvi.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+        uvi.run("app.main:app", host="127.0.0.1", port=8080, reload=True)
     else:
        print ("Server has not been started")
    except Exception as e:
