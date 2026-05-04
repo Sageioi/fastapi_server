ed8c17b5320ebf84ea8db72ff786e3e2c8163de0
Isikuru Oghenetega <tegaisikuru@gmail.com>
2026-05-04 11:27:24 -0900

    Deploying to Render

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 import uvicorn as uvi
 
 def run_server(status = True):
    try:
     if status:
-        uvi.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+        uvi.run("app.main:app", host="0.0.0.0", port=int(os.getenv("PORT")), reload=True)
     else:
        print ("Server has not been started")
    except Exception as e:
